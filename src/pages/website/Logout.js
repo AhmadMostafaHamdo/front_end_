6ad41c902111036ad5d90520be8dc3e69da52906
AhmadHamdo <ahmadhamdo5123@gmail.com>
2024-06-01 03:00:18 +0300

    first commit

---
+++ src/pages/website/Logout.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import '../../styles/logout.css'
+
+export default function Logout () {
+    return (
+        <div className="logout">
+            <div className="logout-pop">
+                <div className='pop-info'>
+                    <p>هل أنت متأكد من تسجيل الخروج ؟</p>
+                    <Link ><button className='ok'
+                    onClick={
+                        ()=> {
+                            window.localStorage.setItem('btnAuth', "تسجيل الدخول")
+                            window.location.pathname = 'front_end_'
+                        }
+                    }
+                    >تأكيد</button></Link>
+                    <Link ><button className='cancel'
+                    onClick={
+                        ()=> {
+                            window.location.pathname = 'front_end_'
+                        }
+                    }
+                    >تراجع</button></Link>
+                </div>
+            </div>
+        </div>
+
+    )
+} 
